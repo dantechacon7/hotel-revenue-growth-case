83eb8f4b748104aeb77bbf7a646ef08a0d061314
Dante Chacon <dantec.chacon@gmail.com>
2026-07-04 16:12:47 -0300

    Update config.py

--- config.py
+++ config.py
@@ -10,21 +10,7 @@ import duckdb
 
 con = duckdb.connect()
 
-## Lendo arquivo
-con.sql("""
-SELECT *
-FROM read_csv_auto('/content/Base Case - base.csv')
-LIMIT 10
-""").df()
-
-## Gerando tabela fato
-con.sql("""
-CREATE TABLE fct_hotel_reservations AS
-SELECT *
-FROM read_csv_auto('/content/Base Case - base.csv')
-""")
-
-## Setup da tabela
+## Setup da tabela fato
 
 !pip install duckdb -q
 import duckdb, pandas as pd
